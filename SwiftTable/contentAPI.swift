282038ee524731ee4938da00efda2f2ba44954a1
resakk611 <ravindran_esakkimuthu@comcast.com>
2020-02-11 15:35:09 +0530

    first commit

---
+++ SwiftTable/contentAPI.swift
@@ -0,0 +1,9 @@
+//
+//  contentAPI.swift
+//  SwiftTable
+//
+//  Created by Ravindran Esakkimuthu on 10/02/20.
+//  Copyright © 2020 Ravindran Esakkimuthu. All rights reserved.
+//
+
+import Foundation
---
+++ SwiftTable/tableCell.swift
@@ -0,0 +1,9 @@
+//
+//  tableCell.swift
+//  SwiftTable
+//
+//  Created by Ravindran Esakkimuthu on 10/02/20.
+//  Copyright © 2020 Ravindran Esakkimuthu. All rights reserved.
+//
+
+import Foundation
